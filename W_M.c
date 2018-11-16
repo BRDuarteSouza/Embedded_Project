58372ab2224545d0890c69bf1510b7740c09151d
Bruno Duarte <brduarte95@gmail.com>
2018-11-16 12:17:32 +0000

    First commit

---
+++ W_M.c
@@ -0,0 +1,7 @@
+// W_M.c
+// Implement a state machine to execute a washing machine taks, with ChibiOS
+// Author: Bruno Duarte <brduart95@gmail.com>
+#include <stdint.h>
+#include <ch.h>
+#include <hal.h>
+#include <chprintf.h>
