5c5ff0409ae3423440c8a1be6a859eb71dac4624
Bruno Duarte <brduarte95@gmail.com>
2018-12-10 12:24:37 +0000

    Exemplo de máquina de estados hierárquica

--- W_M.c
+++ W_M.c
@@ -1,26 +1,36 @@
 // W_M.c
 // Implement a state machine to execute a washing machine taks, with ChibiOS
 // Author: Bruno Duarte <brduart95@gmail.com>
-#include <stdint.h>
+
 #include <ch.h>
 #include <hal.h>
-#include <chprintf.h>
+
+#define RODAR_ESQUERDA 7
+#define RODAR_DIREITA 6
+#define TEMPO_GIRO 1
 
 
 typedef enum {
-  Gira_R,
+  Gira_R = 0,
   Gira_L
 } Giro;
 
+void spining(int time){
+  Giro rodar = Gira_L;
+  long ls_pass;
+  if
+
+  switch(rodar){
+    case(Gira_L){
+      palSetPad(IOPORT4,RODAR_ESQUERDA );
+      if
+    }
+  }
+
+}
 
 
+int main(void){
 
-
-
-
-
-
-void wait_timeout(GPTDriver *gpt_p){
-
-
+  return 0;
 }
